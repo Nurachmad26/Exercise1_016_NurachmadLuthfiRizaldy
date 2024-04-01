faab9ee4b0eebaceee09ce8aa8697a41a9a0cb96
Nurachmad26 <137095395+Nurachmad26@users.noreply.github.com>
2024-04-01 16:00:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link herf="table.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>Table HTML</title>
+  </head>
+  <body>
+    
+  </body>
+    </meta>
+    </link>
+  </head>
+</html>
